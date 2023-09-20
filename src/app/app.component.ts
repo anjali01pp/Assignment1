e83c77c8770eb9341106c49297891bdfcae1f90a
Anjali P P <anjali.p@zerone.training>
2023-09-20 15:35:55 +0530

    Assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'assign1';
   countries: string[] = ["Afghanistan",
