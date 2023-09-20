c19029f9cf58ce0eb7b61749e0e0c2f83611d87f
Anjali P P <anjali.p@zerone.training>
2023-09-20 10:55:20 +0530

    Assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assign1';
+  countries: string[] = ["Afghanistan",
+  "Albania",
+  "Algeria",
+  "Angola",
+  "Argentina",
+  "Australia",
+  "Austria",
+  "Bangladesh",
+  "Belgium",
+  "Brazil",
+  "Canada",
+  "China",
+  "Colombia",
+  "Czech Republic",
+  "Egypt",
+  "France",
+  "Germany",
+  "Greece",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Italy",
+  "Japan",
+  "Kenya",
+  "Mexico",
+  "Netherlands",
+  "New Zealand",
+  "Nigeria",
+  "Norway",
+  "Pakistan",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Russia",
+  "Saudi Arabia",
+  "South Africa",
+  "South Korea",
+  "Spain",
+  "Sweden",
+  "Switzerland",
+  "Thailand",
+  "Turkey",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Venezuela",
+  "Vietnam"
+];
+
+  filteredCountries :string[] = [];
+  filterText: string = '';
+  Filterlength! :number;
+  filterCountries() {
+  this.filteredCountries = this.countries.filter(country =>
+  country.toLowerCase().startsWith(this.filterText.toLowerCase())
+  );
+  this.Filterlength =  this.filteredCountries.length;
+    }
 }
